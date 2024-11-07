f346c12879fb89d9d3cd51fa56c8f428c73ce14e
Jana Saa <jana.abusaa@gmail.com>
2024-11-07 04:05:10 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,22 +1,45 @@
 ﻿using Microsoft.Extensions.Configuration;
-using ServerMonitoringAndNotificationSystem.RabbitMQ;
-using ServerMonitoringAndNotificationSystem.ServerStatistics;
-using System.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ServerMonitoringAndNotificationSystem.Interfaces;
 
 namespace ServerMonitoringAndNotificationSystem
 {
     public class Program
     {
-        private static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder().SetBasePath(Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\"))).AddJsonFile("appsettings.json").Build();
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var config = new ConfigurationBuilder()
+                        .SetBasePath(Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\")))
+                        .AddJsonFile("appsettings.json")
+                        .Build();
 
-            var messageQueueService = new RabbitMQService();
-            var mongoDbService = new MongoDbService(config["MongoDbConnection:ConnectionString"], config["MongoDbConnection:DatabaseName"]);
-            var signalRNotifier = new SignalRNotifier(config["SignalRConfig:SignalRUrl"]);
+                    services.AddSingleton(config);
+                    services.AddRabbitMQClientLibrary(hostContext.Configuration);
+                    services.AddScoped<MessagingService>();
 
-            Console.WriteLine("Service running...");
-            await Task.Delay(Timeout.Infinite);
+                    services.AddScoped<MongoDbService>(sp => new MongoDbService(
+                        config["MongoDbConnection:ConnectionString"],
+                        config["MongoDbConnection:DatabaseName"]));
+
+                    services.AddScoped<AnomalyDetectionConfig>(sp => config.GetSection("AnomalyDetectionConfig").Get<AnomalyDetectionConfig>());
+
+                    services.AddScoped<MessageProcessor>(sp => new MessageProcessor(
+                        sp.GetRequiredService<AnomalyDetectionConfig>(),
+                        sp.GetRequiredService<MongoDbService>(),
+                        sp.GetRequiredService<SignalRNotifier>()));
+
+                    services.AddHostedService<SignalRNotifier>(sp => new SignalRNotifier(config["SignalRConfig:SignalRUrl"]));
+                })
+                .Build();
+
+            var service = host.Services.GetRequiredService<MessagingService>();
+            service.StartListening("MyQueue");
+
+            host.Run();
         }
     }
 }
