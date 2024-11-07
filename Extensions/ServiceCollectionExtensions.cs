568ba65db30851499c85fcf277a5dd07f916f3fd
Jana Saa <jana.abusaa@gmail.com>
2024-11-07 04:01:25 +0200

    Add Extensions folder

---
+++ Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServerMonitoringAndNotificationSystem.Interfaces;
+using ServerMonitoringAndNotificationSystem.RabbitMQ;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddRabbitMQClientLibrary(this IServiceCollection services, IConfiguration configuration)
+    {
+        var rabbitMQConfig = configuration.GetSection("RabbitMQ");
+        string hostName = rabbitMQConfig["HostName"];
+        string exchange = rabbitMQConfig["Exchange"];
+
+        services.AddScoped<IMessagePublisher>(sp => new RabbitMQPublisher(hostName, exchange));
+        services.AddScoped<IMessageConsumer>(sp => new RabbitMQConsumer(hostName));
+
+        return services;
+    }
+}
